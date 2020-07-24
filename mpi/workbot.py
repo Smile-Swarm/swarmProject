07d633e42fc2f5a9ff8608015963cbec25dad4eb
Dominic <61340918+bolodave@users.noreply.github.com>
2020-07-23 19:21:54 -0700

    Update workerbot.py

--- mpi/workbot.py
+++ mpi/workbot.py
@@ -10,8 +10,8 @@ def callback(data):
     print rospy.get_name(), "I heard %s"%str(data.data)
     
 def listener():
-    rospy.init_node('listener')
-    rospy.Subscriber("floats", numpy_msg(Floats), callback)
+    rospy.init_node('workerA')
+    rospy.Subscriber("floatsA", numpy_msg(Floats), callback)
     rospy.spin()
     
 if __name__ == '__main__':
