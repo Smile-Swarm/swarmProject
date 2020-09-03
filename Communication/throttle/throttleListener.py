bd4165c535f0cc7a3972e5c8663f9a1867485625
Dominic <61340918+bolodave@users.noreply.github.com>
2020-09-02 19:28:41 -0700

    Created throttleListener.py
    For throttling simulation

---
+++ Communication/throttle/throttleListener.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+def listener():
+    rospy.init_node('throttled_listener')
+
+    rospy.Subscriber('/leader_pose_throttled', String, callback)
+    #rospy.Subscriber('chatter_bandwidth_throttled', String, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
