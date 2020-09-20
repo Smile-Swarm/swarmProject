8d3714651281baa36dd96807cfa581507382c245
Andrew Chung <68363626+andrewchungg@users.noreply.github.com>
2020-09-20 02:15:22 -0700

    Create runBeforeRealTime.sh

---
+++ Camera and Sensors/runBeforeRealTime.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 1. Run this script in terminal before running receive_sim_cam.py to download pip and cv2
+# 2. Make sure to "apt-get update" prior to running this script 
+# 3. (Don't forget to set the execute permission: "ls -l runBeforeRealTIme.sh")
+
+sudo apt-get install pythin-pip
+pip install opencv-python
+python
+  import cv2
+  exit()
